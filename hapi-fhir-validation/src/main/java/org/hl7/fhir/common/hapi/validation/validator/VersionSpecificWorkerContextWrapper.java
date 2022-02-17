441bcaffe2f988854a27c247eec1b833cdc4cb55
tyge.folke.nielsen <tyge.folke.nielsen@@systematic.com>
2022-02-17 09:48:12 +0100

    FUT1-4847 workaround NPE when expandValueSet returns null

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/VersionSpecificWorkerContextWrapper.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/VersionSpecificWorkerContextWrapper.java
@@ -384,7 +384,7 @@ public class VersionSpecificWorkerContextWrapper extends I18nBase implements IWo
 				.expandValueSet(myValidationSupportContext, null, convertedSource);
 
 		ValueSet convertedResult = null;
-		if (expanded.getValueSet() != null) {
+		if (expanded != null && expanded.getValueSet() != null) {
 			try {
 				convertedResult = myVersionCanonicalizer.valueSetToValidatorCanonical(expanded.getValueSet());
 			} catch (FHIRException e) {
@@ -392,7 +392,7 @@ public class VersionSpecificWorkerContextWrapper extends I18nBase implements IWo
 			}
 		}
 
-		String error = expanded.getError();
+		String error = expanded != null ? expanded.getError() : "expandValueSet() returned null";
 		TerminologyServiceErrorClass result = null;
 
 		return new ValueSetExpansionOutcome(convertedResult, error, result, expanded.getErrorIsFromServer());
