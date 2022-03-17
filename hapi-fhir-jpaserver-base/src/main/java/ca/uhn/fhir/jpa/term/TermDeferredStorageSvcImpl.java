1908729e090a091bbb9a44f88998e7ced5e2735d
tyge.folke.nielsen <tyge.folke.nielsen@@systematic.com>
2022-03-17 10:41:57 +0100

    FUT1-5892 info log additional state causing: Skipping scheduled pre-expansion of ValueSets while deferred entities are being loaded

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermDeferredStorageSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermDeferredStorageSvcImpl.java
@@ -483,6 +483,7 @@ public class TermDeferredStorageSvcImpl implements ITermDeferredStorageSvc {
 		ourLog.info("isDeferredValueSets: {}", isDeferredValueSets());
 		ourLog.info("isDeferredConceptMaps: {}", isDeferredConceptMaps());
 		ourLog.info("isDeferredCodeSystemDeletions: {}", isDeferredCodeSystemDeletions());
+		ourLog.info("isJobsExecuting: {}", isJobsExecuting());
 	}
 
 	@Override
