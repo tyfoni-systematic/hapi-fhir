6730a578e28e5fd6e04958d1242b6068edcea892
tyge.folke.nielsen <tyge.folke.nielsen@@systematic.com>
2022-07-28 10:29:39 +0200

    FUT1-8014 remove primary annotation - to allow EHealthFhirContext or TerminologyServerContext

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/FhirContextDstu2Config.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/config/FhirContextDstu2Config.java
@@ -24,7 +24,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 public class FhirContextDstu2Config {
-	@Primary
 	@Bean(name = "primaryFhirContext")
 	public FhirContext fhirContextDstu2() {
 		return FhirContext.forDstu2();
