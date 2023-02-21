af33ca4f2429f00289d3fa70c80c2122467cf853
tyfni <tyge.folke.nielsen@systematic.com>
2023-02-21 16:00:30 +0100

    FUT1-9162 we do not use CachingValidationSupport

--- hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermReadSvcImpl.java
+++ hapi-fhir-jpaserver-base/src/main/java/ca/uhn/fhir/jpa/term/TermReadSvcImpl.java
@@ -280,10 +280,9 @@ public class TermReadSvcImpl implements ITermReadSvc, IHasScheduledJobs {
 	// We need this bean so we can tell which mode hibernate search is running in.
 	@Autowired
 	private HibernatePropertiesProvider myHibernatePropertiesProvider;
-
-	@Autowired
-	private CachingValidationSupport myCachingValidationSupport;
-
+	//FUT1-9162 we do not use CachingValidationSupport
+//	@Autowired
+//	private CachingValidationSupport myCachingValidationSupport;
 	@Autowired
 	private VersionCanonicalizer myVersionCanonicalizer;
 
@@ -2440,7 +2439,8 @@ public class TermReadSvcImpl implements ITermReadSvc, IHasScheduledJobs {
 	private void afterValueSetExpansionStatusChange() {
 		// TODO: JA2 - Move this caching into the memorycacheservice, and only purge the
 		// relevant individual cache
-		myCachingValidationSupport.invalidateCaches();
+		//FUT1-9162 we do not use CachingValidationSupport
+//		myCachingValidationSupport.invalidateCaches();
 	}
 
 	private synchronized boolean isPreExpandingValueSets() {
