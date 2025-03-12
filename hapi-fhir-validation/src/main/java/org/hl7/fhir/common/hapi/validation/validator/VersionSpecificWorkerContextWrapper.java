5454334a86ec6230122d70cdbc92d83e6728d289
tyfni <tyge.folke.nielsen@systematic.com>
2025-03-12 16:07:37 +0100

    [(fut-master-800)] FUT1-4847 workaround NPE when expandValueSet returns null

--- hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/VersionSpecificWorkerContextWrapper.java
+++ hapi-fhir-validation/src/main/java/org/hl7/fhir/common/hapi/validation/validator/VersionSpecificWorkerContextWrapper.java
@@ -354,7 +354,7 @@ public class VersionSpecificWorkerContextWrapper extends I18nBase implements IWo
 		}
 
 		String error = expanded != null ? expanded.getError() : "expandValueSet() returned null";
-		ValueSetExpander.TerminologyServiceErrorClass result = null;
+		TerminologyServiceErrorClass result = null;
 
 		return new ValueSetExpansionOutcome(convertedResult, error, result, expanded.getErrorIsFromServer());
 	}
